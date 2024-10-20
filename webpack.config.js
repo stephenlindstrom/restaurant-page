59605551ce976c28d628181f8172d3c7f3a2aeb2
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-10-20 10:30:58 -0700

    Add header and image to page

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,21 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html",
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+        ],
+    },
 };
