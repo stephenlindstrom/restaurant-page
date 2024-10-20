01a31575c93943eb3ca00db4fae8ba9832b50c52
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-10-20 14:43:23 -0700

    Create home page through javascript

---
+++ src/home-page.js
@@ -0,0 +1,19 @@
+import burgerImg from "./hamburger.jpg";
+export { homePageLoad };
+
+const homePageLoad = function () {
+    const content = document.querySelector("#content");
+    
+    const pageTitle = document.createElement("h1");
+    pageTitle.textContent = "My Big Restaurant";
+    content.appendChild(pageTitle);
+
+    const image = document.createElement("img");
+    image.src = burgerImg;
+    image.height = 300;
+    content.appendChild(image);
+
+    const description = document.createElement("p");
+    description.textContent = "The top new hamburber restaurant in the Valley!"
+    content.appendChild(description);
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("hello");
+import { homePageLoad } from "./home-page";
+
+homePageLoad();
